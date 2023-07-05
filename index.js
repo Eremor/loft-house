3f33cf3ce5296cd128c3b60694de07a43988d9bf
Eremor <artem.s13@gmail.com>
2023-07-05 15:35:12 +0300

    feat: add no-scroll

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const nav = document.querySelector('.header__nav');
 burgerBtn.addEventListener('click', () => {
   burgerBtn.classList.toggle('burger--open');
   nav.classList.toggle('nav--open');
+  document.body.classList.toggle('no-scroll');
 })
 
 nav.addEventListener('click', (e) => {
@@ -13,5 +14,6 @@ nav.addEventListener('click', (e) => {
   ) {
     burgerBtn.classList.remove('burger--open');
     nav.classList.remove('nav--open');
+    document.body.classList.remove('no-scroll');
   }
 })
